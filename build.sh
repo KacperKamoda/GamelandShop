f1233150011b82d698482cdf1bb0e869d5af9a75
KacperKamoda <kacperkamoda@gmail.com>
2025-09-16 12:17:58 +0200

    build: Final, clean project structure and deployment configuration

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
-cat <<EOF > build.sh
 #!/bin/bash
 set -e
 curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel 9.0 --install-dir .dotnet
 ./.dotnet/dotnet publish GamelandShop.sln -c Release -o output
 cp vercel.json output/wwwroot/
-EOF
